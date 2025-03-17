87b83eab1a1f73ee53cef2cd03108317da1a9ca0
Terry <a23939889231@gmail.com>
2025-03-17 16:14:01 +0800

    input & v-model

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <p v-if="isShow">Hello World</p>
-  <p v-show="isShow">Hello World</p>
-  <p v-for="(user, index) in users1" :key="user">{{ index }} {{ user }}</p>
-  <p v-for="(user, index) in users2" :key="user.email">
-    {{ index }} {{ user.name }} {{ user.email }}
-  </p>
-  <div v-for="(user, index) in users1" :key="user">
-    <p v-if="user === 'Jake'">{{ index }} {{ user }}</p>
-  </div>
-  {{ myFunction(100) }}
+  {{ name }}
+  <br>
+  <input type="text" v-model="name">
+  <br>
+  <input type="text" v-model.trim="name">
+  <br>
+  <input type="text" v-model.lazy="name">
+  <hr>
+  <p v-once>{{ message }}</p>
+  <input type="text" v-model="message" />
+  <hr>
+  <p v-pre>{{ name }}</p>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
@@ -16,23 +19,10 @@ export default {
   name: 'App',
   data() {
     return {
-      isShow: true,
-      users1: ['Jake', 'Allan', 'Eason'],
-      users2: [
-        { name: 'Jake', email: 'jake@gmail.com' },
-        { name: 'Allan', email: 'allan@gmail.com' },
-        { name: 'Eason', email: 'eason@gmail.com' },
-      ],
-      x: 100,
-      y: 2,
-    }
+      name: '',
+      message: 'Hello',
+    };
   },
-  methods: {
-    myFunction(value) {
-      return this.x * this.y + value;
-      //注意在這邊要用 this 來取得定義在 data() 內的變數
-    },
-  }
 };
 </script>
 
