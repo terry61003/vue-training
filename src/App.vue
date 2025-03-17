54cc463cd7fc7b9ed83f5c9214e9922cb28d6a00
Terry <a23939889231@gmail.com>
2025-03-17 18:40:03 +0800

    基本引入和變數

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,31 @@
 <template>
-  <p v-if="isShow">Hello World</p>
-  <p v-show="isShow">Hello World</p>
-  <p v-for="(user, index) in users1" :key="user">{{ index }} {{ user }}</p>
-  <p v-for="(user, index) in users2" :key="user.email">
-    {{ index }} {{ user.name }} {{ user.email }}
-  </p>
-  <div v-for="(user, index) in users1" :key="user">
-    <p v-if="user === 'Jake'">{{ index }} {{ user }}</p>
-  </div>
-  {{ myFunction(100) }}
+  {{ name }}
+  <br>
+  {{ user.id }} {{ user.name }}
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default {
   name: 'App',
-  data() {
+  setup() {
+    const name = ref('');
+    const user = reactive({
+      id: 0,
+      name: '',
+    });
+
+    // 設定值要使用value
+    name.value = 'Jake';
+
+    user.id = 1;
+    user.name = 'Allan';
+
     return {
-      isShow: true,
-      users1: ['Jake', 'Allan', 'Eason'],
-      users2: [
-        { name: 'Jake', email: 'jake@gmail.com' },
-        { name: 'Allan', email: 'allan@gmail.com' },
-        { name: 'Eason', email: 'eason@gmail.com' },
-      ],
-      x: 100,
-      y: 2,
-    }
+      name, user
+    };
   },
-  methods: {
-    myFunction(value) {
-      return this.x * this.y + value;
-      //注意在這邊要用 this 來取得定義在 data() 內的變數
-    },
-  }
 };
 </script>
 
