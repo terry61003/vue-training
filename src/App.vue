4453cb172fadec095a0b67c70595bc41dbd0b11b
Terry <a23939889231@gmail.com>
2025-03-17 16:39:27 +0800

    表單範例

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,35 @@
 <template>
-  {{ name }}
-  <br>
-  <input type="text" v-model="name">
-  <br>
-  <input type="text" v-model.trim="name">
-  <br>
-  <input type="text" v-model.lazy="name">
-  <hr>
-  <p v-once>{{ message }}</p>
-  <input type="text" v-model="message" />
-  <hr>
-  <p v-pre>{{ name }}</p>
-  <input type="text" v-model="name" />
-  <hr>
-  {{ text }} <br />
-  <textarea v-model="text" />
-  <hr>
-  {{ selectValue }} <br />
-  <select v-model="selectValue">
+  <input type="text" v-model="formData.name" />
+  <br />
+
+  <textarea v-model="formData.text" />
+  <br />
+
+  <select v-model="formData.selectValue">
     <option value="">select</option>
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
   </select>
-  <hr>
-  {{ isCheck }} <br />
-  <input type="checkbox" v-model="isCheck" /> is check
-  <hr>
-  {{ checkbox }} <br />
-  <input type="checkbox" value="0" v-model="checkbox" /> 0
-  <input type="checkbox" value="1" v-model="checkbox" /> 1
-  <input type="checkbox" value="2" v-model="checkbox" /> 2
-  <input type="checkbox" value="3" v-model="checkbox" /> 3
-  <hr>
-  {{ radio }} <br />
-  <input type="radio" value="0" v-model="radio" /> 0
-  <input type="radio" value="1" v-model="radio" /> 1
-  <input type="radio" value="2" v-model="radio" /> 2
-  <input type="radio" value="3" v-model="radio" /> 3
+  <br />
+
+  <input type="checkbox" v-model="formData.isCheck" /> is check
+  <br />
+
+  <input type="checkbox" value="0" v-model="formData.checkbox" /> 0
+  <input type="checkbox" value="1" v-model="formData.checkbox" /> 1
+  <input type="checkbox" value="2" v-model="formData.checkbox" /> 2
+  <input type="checkbox" value="3" v-model="formData.checkbox" /> 3
+  <br />
+
+  <input type="radio" value="0" v-model="formData.radio" /> 0
+  <input type="radio" value="1" v-model="formData.radio" /> 1
+  <input type="radio" value="2" v-model="formData.radio" /> 2
+  <input type="radio" value="3" v-model="formData.radio" /> 3
+  <br />
+  <button @click="submit">送出</button>
+  <br>
+  {{ formData }}
 </template>
 
 <script>
@@ -45,15 +37,21 @@ export default {
   name: 'App',
   data() {
     return {
-      name: '',
-      message: 'Hello',
-      text: '',
-      selectValue: '',
-      isCheck: false,
-      checkbox: [],
-      radio: '',
+      formData: {
+        name: '',
+        text: '',
+        selectValue: '',
+        isCheck: false,
+        checkbox: [],
+        radio: '',
+      }
     };
   },
+  methods: {
+    submit() {
+      console.log(this.formData);
+    }
+  }
 };
 </script>
 
