6af3404b123f55adfed4293e98a1defa24ba4390
Terry <a23939889231@gmail.com>
2025-03-17 17:31:15 +0800

    上層元件傳遞資料給下層元件

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,20 @@
 <template>
-  <p v-if="isShow">Hello World</p>
-  <p v-show="isShow">Hello World</p>
-  <p v-for="(user, index) in users1" :key="user">{{ index }} {{ user }}</p>
-  <p v-for="(user, index) in users2" :key="user.email">
-    {{ index }} {{ user.name }} {{ user.email }}
-  </p>
-  <div v-for="(user, index) in users1" :key="user">
-    <p v-if="user === 'Jake'">{{ index }} {{ user }}</p>
-  </div>
-  {{ myFunction(100) }}
+  <Header :name="name" />
 </template>
 
 <script>
+import Header from './components/HeaderTest.vue';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+  },
   data() {
     return {
-      isShow: true,
-      users1: ['Jake', 'Allan', 'Eason'],
-      users2: [
-        { name: 'Jake', email: 'jake@gmail.com' },
-        { name: 'Allan', email: 'allan@gmail.com' },
-        { name: 'Eason', email: 'eason@gmail.com' },
-      ],
-      x: 100,
-      y: 2,
-    }
+      name: 'Allan',
+    };
   },
-  methods: {
-    myFunction(value) {
-      return this.x * this.y + value;
-      //注意在這邊要用 this 來取得定義在 data() 內的變數
-    },
-  }
 };
 </script>
 
---
+++ src/components/HeaderTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello Component</h1>
+    <p>{{ name }}</p>
+    <p>{{ email }}</p>
+</template>
+
+<script>
+export default {
+    name: 'HeaderTest',
+    props: {
+        // 定義型別
+        name: String,
+        email: {
+            type: String,
+            default: 'demo@demo'
+        }
+    },
+};
+</script>
+
+<style></style>
