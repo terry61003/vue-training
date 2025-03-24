e98a3cae3a9e3e108a49b04b4a15abe200949688
Terry <a23939889231@gmail.com>
2025-03-24 14:04:02 +0800

    route pass value

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about?x=aaa">About</router-link> |
+    <router-link :to="{ path: '/about', query: { x: 'xxxx', y: 'yyyy' } }">
+      About Composition
+    </router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{ x }}</h1>
+    <h1>{{ y }}</h1>
   </div>
 </template>
+
+<script>
+import { ref, onMounted, getCurrentInstance } from "vue";
+
+export default {
+  data() {
+    return {
+      x: this.$route.query.x
+    }
+  },
+
+  setup() {
+    let y = ref('');
+
+    onMounted(() => {
+      const instance = getCurrentInstance();
+      y.value = instance.proxy.$route.query.y;
+    });
+
+    return {
+      y
+    };
+  }
+}
+</script>
