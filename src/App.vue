e7b60af77e611e98cece3e6d6cfcd0c7104fa7c7
Terry <a23939889231@gmail.com>
2025-03-24 13:58:42 +0800

    route

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
 <template>
-  <p v-if="isShow">Hello World</p>
-  <p v-show="isShow">Hello World</p>
-  <p v-for="(user, index) in users1" :key="user">{{ index }} {{ user }}</p>
-  <p v-for="(user, index) in users2" :key="user.email">
-    {{ index }} {{ user.name }} {{ user.email }}
-  </p>
-  <div v-for="(user, index) in users1" :key="user">
-    <p v-if="user === 'Jake'">{{ index }} {{ user }}</p>
-  </div>
-  {{ myFunction(100) }}
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      isShow: true,
-      users1: ['Jake', 'Allan', 'Eason'],
-      users2: [
-        { name: 'Jake', email: 'jake@gmail.com' },
-        { name: 'Allan', email: 'allan@gmail.com' },
-        { name: 'Eason', email: 'eason@gmail.com' },
-      ],
-      x: 100,
-      y: 2,
-    }
-  },
-  methods: {
-    myFunction(value) {
-      return this.x * this.y + value;
-      //注意在這邊要用 this 來取得定義在 data() 內的變數
-    },
-  }
-};
-</script>
 
 <style>
 #app {
@@ -43,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
